9ddf9516abffdda05ecbdedc769ce87238215db6
shashi31 <82194005+shashikanth31@users.noreply.github.com>
2021-12-10 19:26:26 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import Button from "./components/Button";
+import   "./index.css"
+
+
+
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  let incrementCount = () => {
+    setCount(count + 1);
+  };
+
+  let decrementCount = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div className="app">
+      <div>
+        <div class="count">
+          <h3>Count:</h3>
+          <h1>{count}</h1>
+        </div>
+        <div class="buttons">
+          <Button title={"-"} action={decrementCount} />
+          <Button title={"+"} action={incrementCount} />
+        </div>
+      </div>
+    </div>
+  );
+}
